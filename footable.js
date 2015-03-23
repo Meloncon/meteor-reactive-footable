e1ace9ddd45416901f4d013da6ff5f136e7c1f21
Matt Meloncon <mmeloncon@gmail.com>
2015-03-23 17:03:01 -0400

    It is working in the simpest of terms. Yay!

--- footable.js
+++ footable.js
@@ -1,16 +1,24 @@
 Template.footable.rendered = function () {
 	var self = this;
 	var dataAttributes = Object.getOwnPropertyNames(Template.currentData());
+	var table = self.find('.footable');
 
 	$.each(dataAttributes, function(index, value) {
-		var table = self.find('.footable');
-
 		$(table).attr(value, Template.currentData()[value]);
 	});
+
+	console.log('Initializing Footables...');
+	$(table).footable();
+};
+
+Template.fooBody.rendered = function () {
+	//console.log('Redrawing Footables');
+	//$('.footable').trigger('footable_redraw');
 };
 
 Template.fooBody.helpers({
 	mapped: function (arr) {
+		console.log(arr);
 		var mappedArray = arr.map(function (item, index) {
 			item.$last = index === arr.length-1;
 			return item;
@@ -19,6 +27,7 @@ Template.fooBody.helpers({
 		return mappedArray || [];
 	},
 	redrawFootable: function () {
-		
+		console.log('Redrawing Footables via Helper');
+		$('.footable').trigger('footable_redraw');
 	}
 });
