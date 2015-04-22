5d028500563c9203d1a429c17bf65ff96eda6ca5
Matt Meloncon <mmeloncon@gmail.com>
2015-04-22 16:52:32 -0400

    Prepping package.js for publishing to Atmosphere.

--- package.js
+++ package.js
@@ -3,11 +3,14 @@ Package.describe({
   version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Responsive HTML Table for Meteor',
+  description: 'Responsive HTML Table for Meteor',
+  homepage: 'https://github.com/Meloncon/meteor-reactive-footable',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Meloncon/meteor-reactive-footable.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
+  author: 'Matt Meloncon <mmeloncon@gmail.com>'
 });
 
 Package.onUse(function(api) {
